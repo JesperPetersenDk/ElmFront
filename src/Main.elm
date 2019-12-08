7fd2075226e48b4ee3da8eb384d88692002ba50a
Mads Wulff Nielsen <mwn@Madss-MacBook-Pro-3.local>
2019-12-08 20:59:21 +0100

    getAllMembers-getMemberById(partly)

--- src/Main.elm
+++ src/Main.elm
@@ -5,97 +5,194 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode exposing (Decoder, field, int, string, string, list, map3)
+import Json.Decode as JD exposing (Decoder, field, int, string, map3, string)
 
-main = Browser.element
-  { init = startIt
-  , update = updateIt
-  , subscriptions = noSubscriptions
-  , view = showIt
+
+
+-- MAIN
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+-- USER
+type alias Member =
+  {
+      id: Int
+      ,firstName: String
+      ,lastName: String
   }
 
-type alias Person =
-  { id: Int
-  , firstName: String
-  , lastName: String
-
-  }
-
-type alias Model =
-  { person: Maybe Person
+type alias ListModel =
+  {
+    memberList: List Member
   , msg: String
   }
 
+type alias Value =
+  { 
+      value : String
+  }
+
+-- MODEL
+type Model
+  = Failure
+  | Loading
+  | Success Member
+  | SuccessAll (List Member)
+
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+    (Loading, getSingleMember("2"))
+  
+
+
+-- UPDATE
 type Msg
-  = FetchPerson1
-  | GotPerson (Result Http.Error Person)
+  = MorePlease
+  | SingleMemberButton
+  | GetAllMembersButton
+  | GotMember (Result Http.Error Member)
+  | GotAllMembersList (Result Http.Error (List Member))
 
 
-startIt: () -> (Model, Cmd Msg)
-startIt _ = (Model Nothing "OK", Cmd.none)
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease ->
+      (Loading, getSingleMember("2"))
 
-updateIt:  Msg -> Model -> (Model, Int -> Cmd Msg)
-updateIt message model =
-  case message of
-    --FetchPerson -> (model, fetchPerson1)
+    SingleMemberButton ->
+      (Loading, getSingleMember("2"))
+    
+    GetAllMembersButton ->
+      (Loading, getAllMembers)
 
-    FetchPerson1 -> (model, fetchPerson1)
+    GotMember result ->
+      case result of
+        Ok url ->
+          (Success url, Cmd.none)
+        Err _ ->
+          (Failure, Cmd.none)
 
-    GotPerson (Ok p) -> ({ model | person = Just p },Cmd.none)
-
-    GotPerson (Err err) -> ({ model | msg = (printError err) }, Cmd.none)
-
-printError: Http.Error -> String
-printError error =
-  case error of
-    Http.BadBody m -> "Bad body "++m
-    Http.BadUrl u -> "Bad URL: "++u
-    Http.Timeout -> "Timeout"
-    Http.NetworkError -> "Network panic"
-    Http.BadStatus i -> "Bad Status: "++(String.fromInt i)
-
-        
-fetchPerson: Cmd Msg
-fetchPerson =
-    Http.get
-        {url = "http://localhost:5000/getmember/1"
-        , expect = Http.expectJson GotPerson personDecoder }
-
--- hvad med fetchPerson: Int -> Cmd Msg
-fetchPerson1: Int -> Cmd Msg
-fetchPerson1 int =
-    Http.get
-        {url = "http://localhost:5000/getmember/" ++ String.fromInt(int)
-        , expect = Http.expectJson GotPerson personDecoder }
+    GotAllMembersList result ->
+      case result of
+        Ok url ->
+          (SuccessAll url, Cmd.none)
+        Err _ ->
+          (Failure, Cmd.none)
 
 
-personDecoder: Decoder Person
-personDecoder =
-    map3 Person
-        (field "id" int)
-        (field "firstName" string)
-        (field "lastName" string)
-
-noSubscriptions: Model -> Sub Msg
-noSubscriptions model =
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
   Sub.none
 
-showIt: Model -> Html Msg
-showIt model =
+
+
+-- VIEW
+view : Model -> Html Msg
+view model =
   div []
-    [ input [ placeholder "Insert ID"] []
-    , button [onClick FetchPerson1] [text "Fetch person"]
-    , br [] []
-    , text ("Person is "++(getName model.person))
-    , br [] []
-    , text model.msg
-    , hr [] []
-   
+    [ hr [] []
+    , searchMember model
+    , viewTable model
     ]
 
-getName: Maybe Person -> String
-getName mp =
-    case mp of 
-        Just person -> person.firstName++" "++(person.lastName)
-        Nothing -> "..."
+searchMember : Model -> Html Msg
+searchMember model = 
+    div []
+        [ 
+        h5 [] [text "Get all members"]
+        , button [ onClick GetAllMembersButton] [ text "Get all" ]
+        , br [] []
+        , h5 [] [text "Get member by id"]
+        , input [type_ "text", placeholder "id" ] []
+        , br [] []
+        , button [ onClick SingleMemberButton] [ text "Get member" ]
+        , hr [] []
+        ]
 
+viewTable : Model -> Html Msg
+viewTable model =
+  case model of
+    Failure ->
+      div []
+        [ text "I could not load any member. " ]
+
+    Loading ->
+      text "Loading..."
+
+    Success user ->
+      div []
+        [ 
+        table [] 
+            [
+            tr []
+                [
+                    th[] [text "ID "]
+                    , th[] [text "Firstname "]
+                    , th[] [text "Lastname "]
+                ]
+            ,tr [] 
+                [
+                    td[] [text (String.fromInt user.id)]
+                    ,td[] [text user.firstName]
+                    ,td[] [text user.lastName]
+                ]
+            ]
+        ]
+    
+    SuccessAll list ->
+      table []
+        ([ tr []
+            [ th [] [ text "ID" ]
+            , th [] [ text "Firstname" ]
+            , th [] [ text "Lastname"]
+            ]
+         ]
+            ++ List.map showUser list
+        )
+
+showUser: Member -> Html Msg
+showUser member =
+    tr [] 
+        [
+            td[] [text (String.fromInt member.id)]
+            ,td[] [text member.firstName]
+            ,td[] [text member.lastName]
+        ]
+
+-- HTTP
+
+getSingleMember : String -> Cmd Msg
+getSingleMember id = 
+    Http.get
+        { url = "http://localhost:5000/getmember/"++id
+        , expect = Http.expectJson GotMember memberDecoder
+        }
+
+getAllMembers : Cmd Msg
+getAllMembers =
+    Http.get
+        { url = "http://localhost:5000/getmembers"
+        , expect = Http.expectJson GotAllMembersList memberListDecoder
+        }
+
+-- JSON DECODERS
+
+memberDecoder: Decoder Member
+memberDecoder = map3 Member (field "id" int) (field "firstName" string) (field "lastName" string)
+
+memberListDecoder: Decoder ( List Member )
+memberListDecoder = JD.list memberDecoder
+  
+
+msgDecoder : Decoder String
+msgDecoder = 
+  field "name" string
